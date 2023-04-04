938f5f465e3ebcb837ac993400dae36a868b45d9
Joshua Chang <32766984+LeanC0de@users.noreply.github.com>
2023-04-05 00:53:35 +0800

    fuction-start

---
+++ 4.4/proto-learn/index.js
@@ -0,0 +1,14 @@
+function Test() {
+    this.a = 1;
+}
+
+
+
+
+console.log('hello');
+console.log(Test.prototype);
+//构造函数有 prototype 属性
+/* 
+构造函数有 prototype 属性，prototype也是一个对象，对象拥有 __proto__
+
+*/
